a8dec13b7046eaed71d4c65864979001289c6447
KOMPUTER\StavroZ <stavrozzz3@gmail.com>
2026-03-08 19:55:41 +0500

    хп врага, урон по врагу, сцена, враг

---
+++ Assets/Student/scripts/EnemyHealth.cs
@@ -0,0 +1,43 @@
+
+using System;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private float maxHealth = 100; // максимальное хп( как раз переменная) публичная, чтобы можно было менять в юнити
+    private float currentHealth; // текущее хп, тоже публичная
+    private Material material;
+    private Color originalColor;  // сохраняю оригинальный цвет
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        originalColor = material.color;  // ← запоминаем оригинальный цвет при старте
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        material.color = Color.red; // краснеет при ударе
+        Invoke("ResetColor", 0.1f); // через 0.1 сек вернуть цвет
+
+        if (currentHealth <= 0) Die();
+    }
+
+    void ResetColor()
+    {
+        material.color = originalColor;  //  возвращаем сохраненный оригинальный цвет
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+   
+}
---
+++ Assets/Student/scripts/PlayerDamage.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace Assets.Student.scripts
+{
+    public class PlayerDamage : MonoBehaviour
+    {
+        // Теперь у нас два поля вместо одного
+        private float minDamage = 1;    // минимальный урон
+        private float maxDamage = 5;    // максимальный урон
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                TryDamageEnemy();
+            }
+        }
+
+        void TryDamageEnemy()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+
+                if (enemyHealth != null)
+                {
+                    // Генерируем случайное число между minDamage и maxDamage
+                    float randomDamage = Random.Range(minDamage, maxDamage + 1);
+                    // +1 нужен, потому что Random.Range для int не включает последнее число
+                    
+
+                    enemyHealth.TakeDamage(randomDamage);
+
+                    // Для отладки — выведем в консоль, какой урон выпал
+                    Debug.Log("Нанесён урон: " + randomDamage);
+                }
+            }
+        }
+    }
+}
